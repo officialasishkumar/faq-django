58648afd359d49ad88f70e9d8777030de8002c9b
Asish Kumar <officialasishkumar@gmail.com>
2025-02-02 13:42:24 +0530

    fix: improve tests
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- faq/tests.py
+++ faq/tests.py
@@ -7,7 +7,11 @@ class FAQModelTest(APITestCase):
     def setUp(self):
         self.faq = FAQ.objects.create(
             question="What is your name?",
-            answer="<p>My name is ChatGPT.</p>"
+            answer="<p>My name is ChatGPT.</p>",
+            question_hi="आपका नाम क्या है?",
+            answer_hi="<p>मेरा नाम चैटजीपीटी है।</p>",
+            question_bn="তোমার নাম কী?",
+            answer_bn="<p>আমার নাম ChatGPT।</p>",
         )
 
     def test_get_translation_default(self):
@@ -17,14 +21,24 @@ class FAQModelTest(APITestCase):
 
     def test_get_translation_hindi(self):
         translation = self.faq.get_translation('hi')
-        self.assertNotEqual(translation['question'], "What is your name?")
-        self.assertTrue(len(translation['question']) > 0)
+        self.assertEqual(translation['question'], "आपका नाम क्या है?")
+        self.assertIn("<p>", translation['answer'])
+
+    def test_get_translation_bengali(self):
+        translation = self.faq.get_translation('bn')
+        self.assertEqual(translation['question'], "তোমার নাম কী?")
+        self.assertIn("<p>", translation['answer'])
+
 
 class FAQAPITest(APITestCase):
     def setUp(self):
         self.faq = FAQ.objects.create(
             question="What is your name?",
-            answer="<p>My name is ChatGPT.</p>"
+            answer="<p>My name is Asish.</p>",
+            question_hi="आपका नाम क्या है?",
+            answer_hi="<p>मेरा नाम असीश है।</p>",
+            question_bn="তোমার নাম কী?",
+            answer_bn="<p>আমার নাম Asish।</p>",
         )
 
     def test_api_get_default(self):
@@ -38,5 +52,10 @@ class FAQAPITest(APITestCase):
         url = reverse('faq-list')
         response = self.client.get(url + '?lang=hi')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertNotEqual(response.data[0]['question'], "What is your name?")
-    
+        self.assertEqual(response.data[0]['question'], "आपका नाम क्या है?")
+
+    def test_api_get_bengali(self):
+        url = reverse('faq-list')
+        response = self.client.get(url + '?lang=bn')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['question'], "তোমার নাম কী?")
